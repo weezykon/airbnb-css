7f3cbc32243ecb7ff26dba23cedfe41952338a11
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2019-12-26 23:48:48 +0100

    airbnb

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
       height: {
         'screen-1/2': '50vh',
         'screen-1/4': '25vh',
+        'screen-30': '30vh',
+        'screen-40': '40vh',
+        'screen-50': '50vh',
         'screen-3/4': '75vh',
         'screen-85': '85vh',
       },
@@ -12,6 +15,8 @@ module.exports = {
         '30': '30%',
         '60': '60%',
         '90': '90%',
+        '48': '48%',
+        'percent-19': '19%',
       },
     }
   },
